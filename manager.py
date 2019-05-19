b39d03e3996e4854b526235b523b45cff562df12
Stephen Chung <spchung95@gmail.com>
2019-05-19 16:28:09 -0700

    naive sync

--- manager.py
+++ manager.py
@@ -1,5 +1,6 @@
 import json
 import os
+import tempfile
 
 import dropbox
 from dropbox.files import WriteMode
@@ -19,3 +20,24 @@ def push(dest=EXTERNAL_TODO_FILEPATH, src=TODO_FILE):
 
 def pull(dest=TODO_FILE, src=EXTERNAL_TODO_FILEPATH):
     dbx.files_download_to_file(dest, src)
+
+def sync(src=TODO_FILE, sync_src=EXTERNAL_TODO_FILEPATH):
+    """
+    WIP: right now all this does is append the 'items' in external storage to the local todo file
+    """
+    external_json = None
+    local_json = None
+
+    # load file stored externally
+    with tempfile.NamedTemporaryFile() as tmpfile:
+        pull(dest=tmpfile.name, src=EXTERNAL_TODO_FILEPATH)
+        external_json = json.load(tmpfile)
+
+    # load file stored locally
+    with open(src, 'r') as f:
+        local_json = json.load(f)
+
+    local_json['items'].extend(external_json['items'])
+
+    with open(src, 'w') as f:
+        json.dump(local_json, f)
